7caaed3b636048bb09313e9063c6472345dbf04c
kamarya <a1534193646@Gmail.com>
2023-01-06 17:28:45 -0500

    Updates done by KA

---
+++ src/KamaryaPractice.java
@@ -0,0 +1,6 @@
+
+public class KamaryaPractice {
+    public static void main(String[] args) {
+        System.out.println("Hope all of us can get a satisfied job as soon as possible^-^");
+    }
+}
